dee60a703797671fdea79ec9e58ae26a36735c1f
sonnypb <jakeblackstock123@gmail.com>
2026-04-04 21:10:52 +0100

    Add DPS Threshold Alerts plugin

--- src/test/java/com/dpsthresholdalerts/DpsThresholdAlertsPluginTest.java
+++ src/test/java/com/dpsthresholdalerts/DpsThresholdAlertsPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.dpsthresholdalerts;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class DpsThresholdAlertsPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(DpsThresholdAlertsPlugin.class);
 		RuneLite.main(args);
 	}
 }
